ea3ee10c498ddb41f664517eec9674e463fc9ae9
VaibhavValere <vaibhavb@valere.io>
2022-09-19 12:14:23 +0530

    changes for app name select

--- template/index.js
+++ template/index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
 
-AppRegistry.registerComponent(appName, () => App);
+const APP_NAME = "ProjectName";
+
+AppRegistry.registerComponent(APP_NAME, () => App);
